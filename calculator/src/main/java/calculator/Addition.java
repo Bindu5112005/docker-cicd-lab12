729ccd58d6e45abead6fdceaf42f214c0380a429
Bindu5112005 <bindukrishnamurthyid@gmail.com>
2026-04-27 21:26:56 +0530

    Delete calculator/src/main/java/calculator directory

--- calculator/src/main/java/calculator/Addition.java
+++
@@ -1,9 +0,0 @@
-package calculator;
-
-public class Addition {
-    public static void main(String[] args) {
-        int a = 5;
-        int b = 3;
-        System.out.println("Sum = " + (a + b));
-    }
-}
