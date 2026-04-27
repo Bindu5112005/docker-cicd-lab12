94a32df7c3956a51630ee89e6e7a4287c7bf8f14
Bindu <bindukrishnamurthyid@gmail.com>
2026-04-27 19:39:15 +0530

    Added Jenkinsfile

---
+++ calculator/src/main/java/calculator/Addition.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        System.out.println("Sum = " + (a + b));
+    }
+}
